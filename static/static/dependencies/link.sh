b9037b72def575a5396df8b37844a2e0e564dec4
huliqun <huliquns@126.com>
2017-03-29 10:09:38 +0800

    fix link script

--- static/static/dependencies/link.sh
+++ static/static/dependencies/link.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-ln -s ../../../../node_modules/store store
-ln -s ../../../../node_modules/jquery jquery
-ln -s ../../../../node_modules/jquery-ui jquery-ui
-ln -s ../../../../node_modules/bootstrap bootstrap
-ln -s ../../../../node_modules/select2 select2
-ln -s ../../../../node_modules/font-awesome font-awesome
-ln -s ../../../../node_modules/bootstrap-table bootstrap-table
-ln -s ../../../../node_modules/bootstrap-datepicker bootstrap-datepicker
-ln -s ../../../../node_modules/bootstrap-daterangepicker bootstrap-daterangepicker
-ln -s ../../../../node_modules/bootstrap-dialog bootstrap-dialog
-ln -s ../../../../node_modules/lightbox2 lightbox2
-ln -s ../../../../node_modules/jquery-slimscroll jquery-slimscroll
-ln -s ../../../../node_modules/x-editable x-editable
-ln -s ../../../../node_modules/jquery-sparkline jquery-sparkline
-ln -s ../../../../node_modules/jvectormap jvectormap
-ln -s ../../../../node_modules/fastclick fastclick
-ln -s ../../../../node_modules/raphael raphael
-ln -s ../../../../node_modules/moment moment
-ln -s ../../../../node_modules/tableexport.jquery.plugin tableexport.jquery.plugin
-ln -s ../../../../node_modules/cropper cropper
-ln -s ../../../../node_modules/crypto-js crypto-js
-ln -s ../../../../node_modules/ckeditor ckeditor
-ln -s ../../../../node_modules/icheck icheck
+rm -rf store;cp -r ../../../frontEnd/node_modules/store store
+rm -rf jquery;cp -r ../../../frontEnd/node_modules/jquery jquery
+rm -rf jquery-ui;cp -r ../../../frontEnd/node_modules/jquery-ui jquery-ui
+rm -rf bootstrap;cp -r ../../../frontEnd/node_modules/bootstrap bootstrap
+rm -rf select2;cp -r ../../../frontEnd/node_modules/select2 select2
+rm -rf font-awesome;cp -r ../../../frontEnd/node_modules/font-awesome font-awesome
+rm -rf bootstrap-table;cp -r ../../../frontEnd/node_modules/bootstrap-table bootstrap-table
+rm -rf bootstrap-datepicker;cp -r ../../../frontEnd/node_modules/bootstrap-datepicker bootstrap-datepicker
+rm -rf bootstrap-daterangepicker;cp -r ../../../frontEnd/node_modules/bootstrap-daterangepicker bootstrap-daterangepicker
+rm -rf bootstrap-dialog;cp -r ../../../frontEnd/node_modules/bootstrap-dialog bootstrap-dialog
+rm -rf lightbox2;cp -r ../../../frontEnd/node_modules/lightbox2 lightbox2
+rm -rf jquery-slimscroll;cp -r ../../../frontEnd/node_modules/jquery-slimscroll jquery-slimscroll
+rm -rf x-editable;cp -r ../../../frontEnd/node_modules/x-editable x-editable
+rm -rf jquery-sparkline;cp -r ../../../frontEnd/node_modules/jquery-sparkline jquery-sparkline
+rm -rf jvectormap;cp -r ../../../frontEnd/node_modules/jvectormap jvectormap
+rm -rf fastclick;cp -r ../../../frontEnd/node_modules/fastclick fastclick
+rm -rf raphael;cp -r ../../../frontEnd/node_modules/raphael raphael
+rm -rf moment;cp -r ../../../frontEnd/node_modules/moment moment
+rm -rf tableexport.jquery.plugin;cp -r ../../../frontEnd/node_modules/tableexport.jquery.plugin tableexport.jquery.plugin
+rm -rf cropper;cp -r ../../../frontEnd/node_modules/cropper cropper
+rm -rf crypto-js;cp -r ../../../frontEnd/node_modules/crypto-js crypto-js
+rm -rf ckeditor;cp -r ../../../frontEnd/node_modules/ckeditor ckeditor
+rm -rf icheck;cp -r ../../../frontEnd/node_modules/icheck icheck
